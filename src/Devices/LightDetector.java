a18547cc2c0a44f7629734665aff1a39571db61a
tymoteuszosik <155182839+tymoteuszosik@users.noreply.github.com>
2025-12-10 12:56:24 +0100

    Create LightDetector.java

---
+++ src/Devices/LightDetector.java
@@ -0,0 +1,28 @@
+package Devices;
+
+public class LightDetector extends SHDevice {
+    private LightState state = LightState.NIGHT;  // Początkowy stan (noc)
+
+    // Enum stanu wykrywacza światła
+    public enum LightState {
+        DAY,   // Dzień
+        NIGHT  // Noc
+    }
+
+    // Konstruktor klasy LightDetector
+    public LightDetector() {
+        // Rejestracja kodów sieciowych
+        registerNetworkCode("GET_STATE", () -> state.name());  // Zwraca stan (DAY lub NIGHT)
+        registerNetworkCode("SET_STATE", (String[] params) -> this.setState(LightState.valueOf(params[0])));  // Ustawia stan
+    }
+
+    // Getter stanu wykrywacza
+    public LightState getState() {
+        return state;
+    }
+
+    // Setter stanu wykrywacza
+    public void setState(LightState state) {
+        this.state = state;
+    }
+}
